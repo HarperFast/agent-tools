bd8561d2756669e595f7d50a123335572ebe81a6
Dawson Toth <dawson@harperdb.io>
2026-04-22 10:45:36 -0400

    fix: Tweak user context tool description

--- tools/getUserContext/serverSchema.ts
+++ tools/getUserContext/serverSchema.ts
@@ -5,7 +5,7 @@ import { inputSchema } from './inputSchema.js';
 export const serverSchema: ServerSideTool<z.infer<typeof inputSchema>> = {
 	name: 'getUserContext',
 	description:
-		'Retrieves the context of what the user is looking at on the screen, such as the current page, ' +
-		'the file they are viewing, the path to that file, and the organization, cluster, and instance details.',
+		'Retrieves the context of what the user is looking at on their screen, such as the current page, ' +
+		'the file they are viewing, the database and table visible to them, that sort of thing.',
 	inputSchema,
 };
